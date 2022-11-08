f0ecd8ee3e33252eabe3158594dc2be6461f0efd
gracekyalo <gracendanu27@gmail.com>
2022-11-08 20:41:33 +0300

    create components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import './App.css';
+import NavBar from './components/NavBar';
+import HomeContainer from './components/HomeContainer';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <Router>
+      <div>
+         <NavBar/>
+      </div>
+      
+        <Routes>
+          <Route exact path="/" element={<HomeContainer/>}></Route>
+          <Route exact path="/about" element={<About/>}></Route>
+
+         
+        </Routes>
+    </Router>
+  </div>
   );
 }
 
