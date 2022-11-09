4ce7e047611fb104e9fa7426c4c82d05648255ae
Diana <diana.wanjiru@student.moringaschool.com>
2022-11-09 11:33:16 +0300

    add reservations

---
+++ src/components/Reservation.js
@@ -0,0 +1,34 @@
+import React from "react";
+//import MenuList from "./MenuList";
+//import About from "./About";
+
+
+function Reservation (){
+   return(
+    <div className="reservations">
+        <h1>Place your reservation here.</h1>
+   <form>
+    
+    <input type='text' placeholder="full name"/>
+    <input type='email' placeholder="enter your email"/>
+    <input type='number' placeholder="enter your phone number"/>
+    <input type='datetime-local' placeholder="enter time and date"/>
+    <input type='submit' value="BOOK"/>
+    
+    
+    
+    
+
+</form>
+</div>
+
+   ) 
+}
+
+export default Reservation;
+
+
+
+
+
+
