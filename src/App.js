26fd1743ee07c3e4a4009370fe85586b6a440a4d
Diana <diana.wanjiru@student.moringaschool.com>
2022-11-10 08:54:02 +0300

    working on the CRUD

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import './App.css';
 import NavBar from './components/NavBar';
@@ -6,7 +6,23 @@ import HomeContainer from './components/HomeContainer';
 import About from './components/About';
 import Reservation from './components/Reservation';
 
+
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:9292/customers/reservations")
+      .then((r) => r.json())
+      .then((messages) => setMessages(messages));
+  }, []);
+
+  function handleAddMessage(newMessage) {
+    setMessages([...messages, newMessage]);
+  }
+
+  function handleDeleteMessage(id) {
+    const updatedMessages = messages.filter((message) => message.id !== id);
+    setMessages(updatedMessages);
   return (
     <div>
     <Router>
@@ -17,7 +33,12 @@ function App() {
         <Routes>
           <Route exact path="/" element={<HomeContainer/>}></Route>
           <Route exact path="/about" element={<About/>}></Route>
-          <Route exact path="/reservation" element={<Reservation/>}></Route>
+          <Route exact path="/MessageList" messages={displayedMssages}
+          onMessageDelete={handleDeleteMessage}
+          onUpdateMessage={handleUpdateMessage} 
+          element={<ReservationList/>}></Route>
+          <Route exact path="/reservation" 
+          onAddMessage={handleAddMessage} element={<Reservation/>}></Route>
          
         </Routes>
     </Router>
@@ -25,4 +46,5 @@ function App() {
   );
 }
 
-export default App;
+
+export default App
--- src/components/Reservation.js
+++ src/components/Reservation.js
@@ -4,23 +4,50 @@ import React from "react";
 //import About from "./About";
 
 
-function Reservation (){
+
+
+
+function Reservation ({onAddMessage}){
+   const [body, setBody] = useState("");
+
+   function handleSubmit(e) {
+      e.preventDefault();
+   
+      fetch("http://localhost:9292/customers/reservations", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: currentUser.username,
+          body: body,
+        }),
+      })
+        .then((r) => r.json())
+        .then((newMessage) => {
+          onAddMessage(newMessage);
+          setBody("");
+        });
+    }
+   
+   
+   
    return(
  <div className="form-container">
 
          <h1>Place your reservation here.</h1>
 
-   <form className="reservations">
+   <form className="reservations" onSubmit={handleSubmit}>
    <label for="fullname">Full Name</label>
-    <input type='text' placeholder="full name"/><br/>
+    <input type='text' placeholder="full name" onChange={(e) => setBody(e.target.value)}/><br/>
     <label for="email">Email</label>
-    <input type='email' placeholder="enter your email"/><br/>
+    <input type='email' placeholder="enter your email" onChange={(e) => setBody(e.target.value)}/><br/>
     <label for="number">Phone Number</label>
-    <input type='number' placeholder="enter your phone number"/><br/>
+    <input type='number' placeholder="enter your phone number" onChange={(e) => setBody(e.target.value)}/><br/>
     <label for="datetime">Date-Time</label>
-    <input type='datetime-local' placeholder="enter time and date"/><br/>
-    <label for="sits">Number of Sits</label>
-    <input type='number' placeholder="enter number of sits"/><br/>
+    <input type='datetime-local' placeholder="enter time and date"  onChange={(e) => setBody(e.target.value)}/><br/>
+    <label for="sits">Number of Seats</label>
+    <input type='number' placeholder="enter number of seats" onChange={(e) => setBody(e.target.value)}/><br/>
     
 
     <div className="buttons">
